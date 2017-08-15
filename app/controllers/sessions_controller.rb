2bf81e427776caccf9cb249a5b3b945e555118c4
shenlinxue <842393426@qq.com>
2017-08-15 21:37:06 +0800

    user new and session new view update ,add user validates message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
 
   def create
   	if login(params[:email], params[:password])
+      flash[:notice] = "sign in succeed!"
   		redirect_to root_path
   	else
+      flash[:notice] = "sign in failed!"
   		redirect_to new_session_path
   	end
   end
 
   def destroy
   	logout
+    flash[:notice] = "sign out succeed!"
   	redirect_to root_path
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,10 @@ class UsersController < ApplicationController
   def create
   		@user = User.new(user_params)
   	if @user.save
+      flash[:notice] = "create new user succeed!"
   		redirect_to new_session_path
   	else
+      flash[:notice] = "create new user failed!"
   		render :new
   	end
   end
