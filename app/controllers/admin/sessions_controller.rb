f24a8b49a831010a1e9e27e9fb30b494556531a9
shenlinxue <842393426@qq.com>
2017-08-23 22:58:42 +0800

    ubuntu update

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -6,10 +6,11 @@ class Admin::SessionsController < Admin::AdminController
 
   def create
   	if login(params[:email], params[:password]) && current_user.is_admin?
-
+      flash[:notice] = "登录成功！"
   		redirect_to admin_root_path
   	else
       logout
+      flash[:notice]= "用户名或密码错误！"
   		redirect_to new_admin_session_path
   	end
   end
